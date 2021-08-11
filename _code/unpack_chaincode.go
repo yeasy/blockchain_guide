0088c30248b8dd239add77ab74418c4508dd37c4
yeasy <yangbaohua@gmail.com>
2021-08-11 11:37:35 -0700

    Support args

--- _code/unpack_chaincode.go
+++ _code/unpack_chaincode.go
@@ -1,33 +1,44 @@
+// Usage: ./prog inputPkg outputPkg
+
 package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"github.com/golang/protobuf/proto"
 	pb "github.com/hyperledger/fabric/protos/peer"
-
-
+	"io/ioutil"
+	"os"
 )
 
-func check(e error) {
+func check(msg string, e error) {
 	if e != nil {
+		fmt.Println(msg)
 		panic(e)
 	}
 }
 
 func main() {
-    chaincodePkg := "./test_cc.V1"
-    chaincodeTar := "./test_cc.tar"
-	ccbytes, err := ioutil.ReadFile(chaincodePkg)
-	check(err)
-	buf := ccbytes
+	if len(os.Args) != 3 {
+		fmt.Printf("Usage: %s inputPkg outputPkg\n", os.Args[0])
+		return
+	}
 
+	inputPkg := os.Args[1]
+	outputPkg := os.Args[2]
+	fmt.Printf("Input pkg=%s, output=%s\n", inputPkg, outputPkg)
+
+	ccbytes, err := ioutil.ReadFile(inputPkg)
+	check("Read file failed", err)
+
+	buf := ccbytes
 	depSpec := &pb.ChaincodeDeploymentSpec{}
 	err = proto.Unmarshal(buf, depSpec)
-	check(err)
+	check("Unmarshal depSpec failed", err)
+
 	fmt.Printf("chaincodeSpec=%+v\n", depSpec.ChaincodeSpec)
 
-    payload := depSpec.CodePackage
-    err = ioutil.WriteFile(chaincodeTar, payload, 0644)
-    check(err)
+	payload := depSpec.CodePackage
+	err = ioutil.WriteFile(outputPkg, payload, 0644)
+	check("Write to file failed", err)
 }
+
